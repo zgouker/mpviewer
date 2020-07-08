295678000b78902c64f45b8e3cac9b662af74299
Zachary Gouker <zachgouker@gmail.com>
2020-07-07 20:44:04 -0400

    Simplify reading the config file

--- utils.py
+++ utils.py
@@ -10,13 +10,7 @@ def get_offsets(game):
     config = configparser.ConfigParser()
     config.read('offsets.ini')
     
-    offset_dict['chara_list'] = config[game]['chara_list'].split(',')
-    offset_dict['chara_offset'] = int(config[game]['char_offset'])
-    offset_dict['coin_offset'] = int(config[game]['coin_offset'])
-    offset_dict['max_offset'] =  = int(config[game]['max_offset'])
-    offset_dict['minigame_offset'] =  = int(config[game]['minigame_offset'])
-    offset_dict['happening_offset'] =  = int(config[game]['happening_offset'])
-    offset_dict['player_offset'] =  = int(config[game]['player_offset'])
+    offset_dict = dict(config.items(game))
     
     return offset_dict
 
